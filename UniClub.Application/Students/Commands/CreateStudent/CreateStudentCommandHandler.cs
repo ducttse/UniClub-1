217cb695309792f7c853d2e1f717ec747a95e899
LongPNH <longpnh2001@gmail.com>
2022-02-08 20:41:00 +0700

    Insert student as a person

--- UniClub.Application/Students/Commands/CreateStudent/CreateStudentCommandHandler.cs
+++ UniClub.Application/Students/Commands/CreateStudent/CreateStudentCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using System.Threading;
 using System.Threading.Tasks;
 using UniClub.Application.Common.Interfaces;
@@ -9,17 +10,22 @@ namespace UniClub.Application.Students.Commands.CreateStudent
 {
     public class CreateStudentCommandHandler : IRequestHandler<CreateStudentCommand, string>
     {
+        private readonly string STUDENT_ROLE = "Student";
         private readonly IIdentityService _identityService;
+        private readonly UserManager<Person> _userManager;
         private readonly IMapper _mapper;
 
-        public CreateStudentCommandHandler(IIdentityService identityService, IMapper mapper)
+        public CreateStudentCommandHandler(IIdentityService identityService, UserManager<Person> userManager, IMapper mapper)
         {
             _identityService = identityService;
+            _userManager = userManager;
             _mapper = mapper;
         }
         public async Task<string> Handle(CreateStudentCommand request, CancellationToken cancellationToken)
         {
-            var result = await _identityService.CreateUserAsync(_mapper.Map<Person>(request), request.Password);
+            var student = _mapper.Map<Person>(request);
+            var result = await _identityService.CreateUserAsync(student, request.Password);
+            await _userManager.AddToRoleAsync(student, STUDENT_ROLE);
             return result.UserId;
         }
     }
