558e860d660403910a3a35ddc381b87308c066e8
LongPNH <longpnh2001@gmail.com>
2022-02-25 11:15:40 +0700

    Add CreationTime to PostDto

--- UniClub.Dtos/Response/PostDto.cs
+++ UniClub.Dtos/Response/PostDto.cs
@@ -1,14 +1,17 @@
-﻿using UniClub.Domain.Common.Enums;
+﻿using System;
+using UniClub.Domain.Common.Enums;
 
 namespace UniClub.Dtos.Response
 {
     public class PostDto
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public string PersonId { get; set; }
         public string Content { get; set; }
         public PostStatus Status { get; set; }
         public string ShortDescription { get; set; }
         public int? EventId { get; set; }
+        public DateTime CreationTime { get; set; }
     }
 }
